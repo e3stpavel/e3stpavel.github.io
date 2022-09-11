fe03bc457cb2ee718b4c1f28bec898bfeeb9c63d
e3stpavel <70956582+e3stpavel@users.noreply.github.com>
2022-09-11 17:37:06 +0300

    fix: added import aliases to Vite config

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,14 @@
+import * as path from 'path'
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {},
+  vite: {
+    resolve: {
+      alias: {
+        '~/': `${path.resolve(__dirname, 'src')}/`,
+      },
+    },
+  },
 })
 
